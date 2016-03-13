e03613a509d170f14212e29029bad363ea2d4449
dmr0 <dmr0@users.noreply.github.com>
2016-03-13 14:33:54 +0300

    new web application project

---
+++ src/dmr0/gcp_test/Gcp_testServlet.java
@@ -0,0 +1,13 @@
+package dmr0.gcp_test;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Gcp_testServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
